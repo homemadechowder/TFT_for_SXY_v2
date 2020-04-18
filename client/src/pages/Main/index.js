b4eb4f3b8ea249dd71b9554bc1ed5ed3e33fef31
Andrew Lu <awclu@ucdavis.edu>
2020-04-18 01:03:45 -0700

    phase 1 done

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -11,9 +11,294 @@ class ItemTable extends Component{
         };
     }
 
+    setBackground = () => {
+        
+    }
+
     handleClick = (event) =>{
-        // name = event.target.name;
+        const id = event.target.id;
         // value = event.target.value;
+
+        if (this.state.item1 === '' && this.state.item2 === ''){
+            this.setState({
+                item1:id
+            }, () => {
+                this.handleEquation(this.state.item1, this.state.item2);
+            });
+
+        }
+        else if (this.state.item1 !== '' && this.state.item2 === ''){
+            this.setState({
+                item2:id
+            }, () => {
+                this.handleEquation(this.state.item1, this.state.item2);
+            });
+           
+        }
+        else{
+            this.setState({
+                item1: id,
+                item2: '',
+                itemSum: ''
+            }, () =>{
+                this.handleEquation(this.state.item1, this.state.item2);
+            });
+
+        }
+       
+    }
+
+    handleEquation = (item1, item2) =>{
+        if (item1 === 'bfsword' || item2 === 'bfsword'){
+            if(item1 === 'bfsword' && item2 ==='bfsword'){
+                this.setState({
+                    itemSum: 'deathblade',
+                }) 
+            }  
+            else if(item1 ==='vest' || item2 ==='vest'){
+                this.setState({
+                    itemSum: 'GA',
+                })    
+            }   
+            else if(item1 ==='belt' || item2 ==='belt'){
+                this.setState({
+                    itemSum: 'zeke',
+                })    
+            }   
+            else if(item1 ==='stick' || item2 ==='stick'){
+                this.setState({
+                    itemSum: 'gunblade',
+                })    
+            }   
+            else if(item1 ==='cloak' || item2 ==='cloak'){
+                this.setState({
+                    itemSum: 'bloodthirster',
+                })    
+            }   
+            else if(item1 ==='bow' || item2 ==='bow'){
+                this.setState({
+                    itemSum: 'giantkill',
+                })    
+            }   
+            else if(item1 ==='gold' || item2 ==='gold'){
+                this.setState({
+                    itemSum: 'blademaster',
+                })    
+            }   
+            else if(item1 ==='tear' || item2 ==='tear'){
+                this.setState({
+                    itemSum: 'shojin',
+                })    
+            }      
+            else if(item1 ==='glove' || item2 ==='glove'){
+                this.setState({
+                    itemSum: 'infinity',
+                })    
+            }   
+        }
+
+        else if (item1 === 'vest' || item2 === 'vest') {
+            if(item1 ==='vest' && item2 ==='vest'){
+                this.setState({
+                    itemSum: 'bramble',
+                })    
+            }   
+            else if(item1 ==='belt' || item2 ==='belt'){
+                this.setState({
+                    itemSum: 'redbuff',
+                })    
+            } 
+            else if(item1 ==='stick' || item2 ==='stick'){
+                this.setState({
+                    itemSum: 'solari',
+                })    
+            }   
+            else if(item1 ==='cloak' || item2 ==='cloak'){
+                this.setState({
+                    itemSum: 'swordbreaker',
+                })    
+            }   
+            else if(item1 ==='bow' || item2 ==='bow'){
+                this.setState({
+                    itemSum: 'titanresolve',
+                })    
+            }   
+            else if(item1 ==='gold' || item2 ==='gold'){
+                this.setState({
+                    itemSum: 'rebel',
+                })    
+            }   
+            else if(item1 ==='tear' || item2 ==='tear'){
+                this.setState({
+                    itemSum: 'frozen',
+                })    
+            }      
+            else if(item1 ==='glove' || item2 ==='glove'){
+                this.setState({
+                    itemSum: 'shroudsad',
+                })    
+            }  
+        }
+
+        else if (item1 === 'belt' || item2 === 'belt') { 
+            if(item1 ==='belt' || item2 ==='belt'){
+                this.setState({
+                    itemSum: 'warmog',
+                })    
+            } 
+            else if(item1 ==='stick' || item2 ==='stick'){
+                this.setState({
+                    itemSum: 'morello',
+                })    
+            }   
+            else if(item1 ==='cloak' || item2 ==='cloak'){
+                this.setState({
+                    itemSum: 'zephyr',
+                })    
+            }   
+            else if(item1 ==='bow' || item2 ==='bow'){
+                this.setState({
+                    itemSum: 'rot',
+                })    
+            }   
+            else if(item1 ==='gold' || item2 ==='gold'){
+                this.setState({
+                    itemSum: 'protector',
+                })    
+            }   
+            else if(item1 ==='tear' || item2 ==='tear'){
+                this.setState({
+                    itemSum: 'redemption',
+                })    
+            }      
+            else if(item1 ==='glove' || item2 ==='glove'){
+                this.setState({
+                    itemSum: 'trapclaw',
+                })    
+            }  
+        }
+
+        else if (item1 === 'stick' || item2 === 'stick'){
+            if(item1 ==='stick' && item2 ==='stick'){
+                this.setState({
+                    itemSum: 'hat',
+                })    
+            }   
+            else if(item1 ==='cloak' || item2 ==='cloak'){
+                this.setState({
+                    itemSum: 'ionic',
+                })    
+            }   
+            else if(item1 ==='bow' || item2 ==='bow'){
+                this.setState({
+                    itemSum: 'guinsoo',
+                })    
+            }   
+            else if(item1 ==='gold' || item2 ==='gold'){
+                this.setState({
+                    itemSum: 'demo',
+                })    
+            }   
+            else if(item1 ==='tear' || item2 ==='tear'){
+                this.setState({
+                    itemSum: 'luden',
+                })    
+            }      
+            else if(item1 ==='glove' || item2 ==='glove'){
+                this.setState({
+                    itemSum: 'jeweled',
+                })    
+            }  
+        }
+
+        else if (item1 === 'cloak' || item2 === 'cloak') {
+            if(item1 ==='cloak' && item2 ==='cloak'){
+                this.setState({
+                    itemSum: 'dclaw',
+                })    
+            }   
+            else if(item1 ==='bow' || item2 ==='bow'){
+                this.setState({
+                    itemSum: 'runan',
+                })    
+            }   
+            else if(item1 ==='gold' || item2 ==='gold'){
+                this.setState({
+                    itemSum: 'celestial',
+                })    
+            }   
+            else if(item1 ==='tear' || item2 ==='tear'){
+                this.setState({
+                    itemSum: 'icecream',
+                })    
+            }      
+            else if(item1 ==='glove' || item2 ==='glove'){
+                this.setState({
+                    itemSum: 'quicksilver',
+                })    
+            }  
+        }
+        else if (item1 === 'bow' || item2 === 'bow'){
+            if(item1 ==='bow' && item2 ==='bow'){
+                this.setState({
+                    itemSum: 'rpc',
+                })    
+            }   
+            else if(item1 ==='gold' || item2 ==='gold'){
+                this.setState({
+                    itemSum: 'infiltrator',
+                })    
+            }   
+            else if(item1 ==='tear' || item2 ==='tear'){
+                this.setState({
+                    itemSum: 'static',
+                })    
+            }      
+            else if(item1 ==='glove' || item2 ==='glove'){
+                this.setState({
+                    itemSum: 'lastwhisper',
+                })    
+            }  
+        }
+
+        else if (item1 === 'gold' || item2 === 'gold'){
+            if(item1 ==='gold' && item2 ==='gold'){
+                this.setState({
+                    itemSum: 'fon',
+                })    
+            }   
+            else if(item1 ==='tear' || item2 ==='tear'){
+                this.setState({
+                    itemSum: 'sg',
+                })    
+            }      
+            else if(item1 ==='glove' || item2 ==='glove'){
+                this.setState({
+                    itemSum: 'darkstar',
+                })    
+            }  
+        }
+
+        else if (item1 === 'tear' || item2 === 'tear'){
+            if(item1 ==='tear' && item2 ==='tear'){
+                this.setState({
+                    itemSum: 'seraph',
+                })    
+            }      
+            else if(item1 ==='glove' || item2 ==='glove'){
+                this.setState({
+                    itemSum: 'handofjustice',
+                })    
+            }  
+        }
+
+        else if (item1 === 'glove' || item2 === 'glove'){   
+            if(item1 ==='glove' && item2 ==='glove'){
+                this.setState({
+                    itemSum: 'thieve',
+                })    
+            }  
+        }
     }
 
 
@@ -21,35 +306,26 @@ class ItemTable extends Component{
         return(
             <div className = 'container'>
                 <div className = 'equation'>
-                    <div className = 'item1'>
-                        <img src = {this.state.item1} alt={this.state.item1}>
-
-                        </img>
+                    <div id = {this.state.item1} className = 'item1'>
                     </div>
-            +
-                    <div className = 'item2'>
-                        <img src = {this.state.item2} alt={this.state.item2}>
-                    
-                        </img>
+                    +
+                    <div id = {this.state.item2} className = 'item2'>
                     </div>
-            =
-                    <div className = 'item3'>
-                        <img src = {this.state.item3} alt={this.state.item3}>
-                    
-                        </img>
+                    =
+                    <div id = {this.state.itemSum} className = 'item3'>
                     </div>
                 </div>
 
                 <div className = 'selection'>
-                    <div id = 'bfsword' className ='items'></div>
-                    <div id = 'stick' className ='items'></div>
-                    <div id = 'tear' className ='items'></div>
-                    <div id = 'bow' className ='items'></div>
-                    <div id = 'glove' className ='items'></div>
-                    <div id = 'belt' className ='items'></div>
-                    <div id = 'vest' className ='items'></div>
-                    <div id = 'cloak' className ='items'></div>
-                    <div id = 'gold' className ='items'></div>
+                    <div onClick = {this.handleClick} id = 'bfsword' item = 'bfsword' className = 'items'></div>
+                    <div onClick = {this.handleClick} id = 'stick' item = 'stick' className = 'items'></div>
+                    <div onClick = {this.handleClick} id = 'tear' item = 'tear' className = 'items'></div>
+                    <div onClick = {this.handleClick} id = 'bow' item = 'bow' className = 'items'></div>
+                    <div onClick = {this.handleClick} id = 'glove' item = 'glove' className = 'items'></div>
+                    <div onClick = {this.handleClick} id = 'belt' item = 'belt' className = 'items'></div>
+                    <div onClick = {this.handleClick} id = 'vest' item = 'vest' className = 'items'></div>
+                    <div onClick = {this.handleClick} id = 'cloak' item = 'cloak' className = 'items'></div>
+                    <div onClick = {this.handleClick} id = 'gold' item = 'gold' className = 'items'></div>
                 </div>
             </div>
             
